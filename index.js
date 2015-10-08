90ca81ed165db2caf1174589b7ab66ec38922fdd
Andrew Shaffer <andrew@weo.io>
2015-10-07 18:31:19 -0700

    fix firefox

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ module.exports = function(filename, data) {
   var link = document.createElement('a')
   link.setAttribute('href', encodeURI(data))
   link.setAttribute('download', filename)
+  document.body.appendChild(link)
   link.click()
+  document.body.removeChild(link)
 }
