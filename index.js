e60261652d938663eb784b1e253113a774850f19
Andrew Shaffer <andrew@weo.io>
2015-10-08 12:04:46 -0700

    add IE support

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 module.exports = function(filename, data) {
-  var link = document.createElement('a')
-  link.setAttribute('href', encodeURI(data))
-  link.setAttribute('download', filename)
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
+  if (isMsie()) {
+    var blob = new Blob([data], {type: 'application/csv;charset=utf-8;'})
+    navigator.msSaveBlob(blob, filename)
+  } else {
+    var link = document.createElement('a')
+    link.setAttribute('href', encodeURI(data))
+    link.setAttribute('download', filename)
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }
+}
+
+function isMsie() {
+  return uaContains('MSIE ') || uaContains('Trident/') || uaContains('Edge/')
+}
+
+function uaContains(str) {
+  return window.navigator.userAgent.indexOf(str) >= 0
 }
